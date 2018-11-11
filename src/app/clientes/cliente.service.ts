d357159bd9d1078b515b0c605f73f2b1a5b71f89
ctoribio <ctoribio@coordinatedhealth.com>
2018-11-11 02:10:00 -0500

    ftr-crud-rest - Adding Http Client to consume the server API

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -4,11 +4,19 @@ import { Cliente } from './cliente.js';
 // import { Observable } from 'rxjs/Observable';
 import { of, Observable } from 'rxjs';
 // import { of } from 'rxjs/observable/of';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class ClienteService {
 
-  constructor() { }
+  private urlEndPoint = 'http://localhost:8080/api/clientes';
+  constructor(private http: HttpClient) { }
 
-  getClientes(): Observable<Cliente[]> { return of(CLIENTES); }
+  getClientes(): Observable<Cliente[]> {
+    return this.http.get(this.urlEndPoint).pipe(
+      map( response => response as Cliente[])
+    );
+    // return this.http.get<Cliente[]>(this.urlEndPoint);     //just another  way to do it.
+  }
 }
