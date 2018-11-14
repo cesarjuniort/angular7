d1d69b46b0fca0d0b7b6f96b7ddf79e989873c37
Cesar Toribio <ctoribio@coordinatedhealth.com>
2018-11-14 11:36:02 -0500

    ftr-crud-rest - Added sweetalert2 to replace as the dialogs library, applied css enhancements to the footer, added crud capabilities to cliente.service, the cliente.component display a notification if there is no data.

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
 import { ClienteService } from './cliente.service';
+import swal, { SweetAlertOptions, SweetAlertResult, SweetAlertType } from 'sweetalert2';
 
 
 @Component({
@@ -15,8 +16,48 @@ export class ClientesComponent implements OnInit {
   ngOnInit() {
     // this.clientes = this.clienteService.getClientes();
     this.clienteService.getClientes().subscribe(
-       clientes => this.clientes = clientes
+       apiCliente => this.clientes = apiCliente
     );
+  }
+
+  delete(cliente: Cliente): void {
+    // this.clienteService.delete(cliente.id);
+
+    const swalWithBootstrapButtons = swal.mixin({
+      confirmButtonClass: 'btn btn-success',
+      cancelButtonClass: 'btn btn-danger',
+      buttonsStyling: false,
+    });
+
+    swalWithBootstrapButtons({
+      title: 'Are you sure?',
+      text: 'You won\'t be able to revert this!',
+      type: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Yes, delete it!',
+      cancelButtonText: 'No, cancel!',
+      reverseButtons: true
+    }).then((result) => {
+      if (result.value) {
+        this.clienteService.delete(cliente.id).subscribe(
+          r => {
+            swalWithBootstrapButtons(
+                'Deleted!',
+                'The record has been deleted.',
+                'success'
+              );
+          }
+        );
+      } else if ( result.dismiss === swal.DismissReason.cancel) {     // Read more about handling dismissals
+        swalWithBootstrapButtons(
+          'Cancelled',
+          'Operation cancelled.',
+          'error'
+        );
+      }
+    });
+
+
   }
 
 }
--- src/app/clientes/form.component.ts
+++ src/app/clientes/form.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
+import { ClienteService } from './cliente.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import swal, { SweetAlertOptions, SweetAlertResult, SweetAlertType } from 'sweetalert2';
 
 @Component({
   selector: 'app-form',
@@ -10,14 +13,57 @@ export class FormComponent implements OnInit {
   private cliente: Cliente = new Cliente();
   private titulo = 'Crear Cliente';
 
-  constructor() { }
+  constructor(private clienteSvr: ClienteService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+    ) { }
 
   ngOnInit() {
+    this.loadCustomer();
   }
 
   public create(): void {
-    console.log('created Method called.');
+    this.clienteSvr.create(this.cliente).subscribe(
+      response => {
+        this.navigateToList(); // this.router.navigate(['/clientes']);
+        this.showToast('Saved successfully!');
+      }
+    );
     console.log(this.cliente);
   }
 
+  private showToast(msg: string, toastType: SweetAlertType = 'success') {
+    const toast = swal.mixin({
+      toast: true,
+      position: 'top-end',
+      showConfirmButton: false,
+      timer: 3000
+    });
+    toast({
+      type: toastType,
+      title: msg
+    });
+  }
+
+  loadCustomer(): void {
+    this.activatedRoute.params.subscribe(params => {
+      const id = params['id'];
+      if (id) {
+        this.clienteSvr.getCliente(id).subscribe( (c) => this.cliente = c);
+      }
+    });
+  }
+
+  navigateToList(): void {
+    this.router.navigate(['/clientes']);
+  }
+
+  update(): void {
+    this.clienteSvr.update(this.cliente).subscribe(
+      resp => {
+        this.navigateToList();
+        this.showToast('Updated successfully!');
+      }
+    );
+  }
 }
